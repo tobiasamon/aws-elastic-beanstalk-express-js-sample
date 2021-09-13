cc0c2494fb2a775390049bea829aa1bd7d119164
Tobias Amon <tobias.amon@mhp.com>
2021-09-13 13:35:02 +0200

    Changed Text

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello My New World!'));
+app.get('/', (req, res) => res.send('Modified and automagically deployed!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
